4e974a01453242013d7c568af388e68156566b06
dlekguswkd <dlekguswkd@naver.com>
2024-06-27 18:10:33 +0900

    학원에서 과제 마지막

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -7,16 +7,21 @@ public class Ex17 {
 	public static void main(String[] args) {
 		
 		// 숫자 하나를 입력받아 아래와 같이 * 가 출력되도록 프로그램을 작성하세요.
+		//모르겠음
 
 		Scanner sc= new Scanner(System.in);
 		
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
 		
-		for (int i=1; i<=num; i--) {
+		for (int i=0; i<=num; i++) {
+			int j = num-i ;
+			for (j=num-i;  j>0; j--) {
 			System.out.print("*");
+			}
 		}System.out.println("");
-		
+		sc.close();
 	}
-
+		
+		
 }
